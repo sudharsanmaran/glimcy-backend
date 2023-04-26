341925370dd25cc4837f921e401531906733d3aa
sudharsanmaran <smsudharsanmaran@gmail.com>
2023-04-26 14:22:32 +0530

    for heroku build

--- release-tasks.sh
+++ release-tasks.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-pip install -r backend/requirements/local.txt
 
-python backend/manage.py makemigrations
+python manage.py makemigrations
 
-python backend/manage.py migrate
+python manage.py migrate
 
 #custom Python script to  populate celery intervals and tasks
-python backend/manage.py populate_celery_beat_tables
+python manage.py populate_celery_beat_tables
