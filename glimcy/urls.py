7b26ff332d2aca3eef771c63dabea365ccd6541a
sudharsanmaran <smsudharsanmaran@gmail.com>
2023-04-28 19:28:44 +0530

    gdsf

--- glimcy/urls.py
+++ glimcy/urls.py
@@ -12,7 +12,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-#try
+#tr gbdfbb 
+
+# bgfbn
 
 
 from django.contrib import admin
